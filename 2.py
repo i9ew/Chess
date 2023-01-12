544733d37bd4102a796891051d15d1738aa9205d
qwerty12319 <113201299+qwerty12319@users.noreply.github.com>
2023-01-12 21:47:37 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,49 +1,49 @@
 import sqlite3
 
 
-mail, password, login = input(), input(), input()
-
-
-def duplicate(mail):
-    base = sqlite3.connect('sqlbase')
-    cur = base.cursor()
-    table = cur.execute("""SELECT mail FROM players""")
-    table = [i[0] for i in table]
-    if mail in table:
-        base.close()
-        print('Почта занята')
-        return False
-    base.close()
-    return True
-
-
-def chek(mail):
-    if '@' not in mail or '.' not in mail:
-        print('Введите почту')
-        return False
-    if mail.index('.') - mail.index('@') < 2 or mail.index('@') == 0 or mail.index('.') + 1 == len(mail):
-        print('Введите почту')
-        return False
-    for i in mail:
-        if (i != '!' and not i.isalpha() and i != '@' and not i.isdigit() and i != '.' or mail.count('@') > 1 or
-                mail.count('.') > 1):
-            print('Введите почту')
-            return False
-    if len(password) < 8:
-        print('Плохой пароль')
-        return False
-    return True
-
-
 def registration(mail, password, login):
-    if duplicate(mail) and chek(mail):
+    
+    def duplicate(mail):
+        base = sqlite3.connect('sqlbase')
+        cur = base.cursor()
+        table = cur.execute("""SELECT mail FROM players""")
+        table = [i[0] for i in table]
+        if mail in table:
+            base.close()
+            return False
+        base.close()
+        return True
+
+    def chek1(mail):
+        if '@' not in mail or '.' not in mail:
+            return False
+        if mail.index('.') - mail.index('@') < 2 or mail.index('@') == 0 or mail.index('.') + 1 == len(mail):
+            return False
+        for i in mail:
+            if (i != '!' and not i.isalpha() and i != '@' and not i.isdigit() and i != '.' or mail.count('@') > 1 or
+                    mail.count('.') > 1):
+                return False
+        return True
+            
+    def chek2(password):
+        if len(password) < 8:
+            return False
+        return True
+
+    if duplicate(mail) and chek1(mail) and chek2(password):
         base = sqlite3.connect('sqlbase')
         cur = base.cursor()
         cur.execute("""INSERT INTO players (mail, password, login, settings) VALUES 
                         (?, ?, ?, ?)""", [mail, password, login, ''])
         base.commit()
         base.close()
-        print('Успешно')
+        return 'Успешно'
+    elif not duplicate(mail):
+        return 'Почта уже есть'
+    elif not chek1(mail):
+        return 'Почта бяка'
+    else:
+        return 'Короткий пароль'
 
 
 def vhod(mail, password):
@@ -54,8 +54,8 @@ def vhod(mail, password):
     inf1 = [[str(i[0]), str(i[1])] for i in information]
     inf2 = [[str(i[2]), str(i[1])] for i in information]
     if [mail, password] not in inf1 and [mail, password] not in inf2:
-        print('Пользователь не найден')
-    else:
+        return 'Пользователь не найден'
+    else:      
         if [mail, password] in inf1:
             flag = inf1.index([mail, password])
         else:
@@ -67,6 +67,4 @@ def vhod(mail, password):
 def razlogin():
     with open('username.txt', 'w') as f:
         print(f'user=None', file=f)
-
-
-vhod(mail, password)
+        
