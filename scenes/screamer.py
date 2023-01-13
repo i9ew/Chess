282e924898e3791e6490b928ba4c3610eff1cf6d
NikNov <niknovwinner@gmail.com>
2023-01-13 19:12:11 +0300

    final vers

--- scenes/screamer.py
+++ scenes/screamer.py
@@ -1,11 +1,13 @@
 from classes.Scene import Scene
 import pygame
 from functions import *
+from constants import *
 
 
 class Screamer(Scene):
     def __init__(self):
         super().__init__()
+        self.bg_color = ColoursRGB.RED
         self.bg_image_path = create_full_path(f"data/assets/images/screamer.png")
 
     def input_processing(self, events, events_p):
